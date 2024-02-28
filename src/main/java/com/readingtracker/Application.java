08e7d1bbae5d3723a43be2d5fa4e3000a1a0f56b
Jan Cooper <jan.cooper@live.com>
2024-02-27 22:49:21 -0500

    add datasource and secret encryption

--- src/main/java/com/readingtracker/Application.java
+++ src/main/java/com/readingtracker/Application.java
@@ -1,11 +1,13 @@
 package com.readingtracker;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.*;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
